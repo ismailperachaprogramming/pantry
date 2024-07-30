1445ee0331590bf7b20b12c017c40d3fd5675416
Ismail Peracha <91555891+ismailperachaprogramming@users.noreply.github.com>
2024-07-29 19:01:47 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# pantry
+<<<<<<< HEAD
+
+=======
+# headstarteraiweek2
+>>>>>>> d207fa1f (Initial commit)
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLMyx7w6QA5KX5-4wP7VujiRzYxR1vMOU",
+  authDomain: "hspantryapp-c482d.firebaseapp.com",
+  projectId: "hspantryapp-c482d",
+  storageBucket: "hspantryapp-c482d.appspot.com",
+  messagingSenderId: "1049855256156",
+  appId: "1:1049855256156:web:47128db0a2e8f788b6a899"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
